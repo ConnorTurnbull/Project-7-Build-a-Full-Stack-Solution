8c48f131ada24fd86ebae2c42de94a040c148a1f
Connor Turnbull <connorturnbull93@gmail.com>
2023-05-05 23:07:40 +0100

    app update
    main header added
    side navbar added

--- src/App.js
+++ src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
+//import logo from '';
+import React from 'react';
 import './App.css';
+import SideNav from './components/sideNav';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import MainLogo from './icon-left-font-monochrome-black.svg';
+import { Navbar, Button, Col, Row, Container, } from 'react-bootstrap';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app' >
+      <Container className="header bg-primary" fluid >
+        <Row className='p-3'>
+          
+          <Col md ={2} lg={2} xl={2}>
+            <img className='img-fluid' src={MainLogo} alt="The Groupomania company logo"/>
+          </Col>
+
+          <Col md={8} lg={8} xl={8}>
+
+          </Col>
+
+          <Col md={1} lg={1} xl={1}>
+            <Button className='p-1 text-nowrap' variant='light' size='sm'>Log In</Button>
+          </Col>
+
+          <Col md={1} lg={1} xl={1}>
+            <Button className='p-1 text-nowrap' variant='light' size='sm'>Sign Up</Button>
+          </Col>
+        
+        </Row>
+      </Container>
+
+      <Container className="bg-secondary m-0" style={{width: '20%'}}>
+          <SideNav/>
+      </Container>
     </div>
   );
 }
---
+++ src/components/sideNav.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const SideNav = () => {
+    return (
+        <div className="sideNav vstack gap-3">
+            <h3>Threads</h3>
+            <a href="#">Thread 2</a>
+            <a href="#">Thread 3</a>
+            <a href="#">Thread 1</a>
+            <a href="#">Thread 4</a>
+        </div>
+    )
+}
+
+export default SideNav;
