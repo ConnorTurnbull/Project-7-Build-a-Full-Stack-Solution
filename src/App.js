c7160fee41990a2b253c18435bf7cea1fbc42524
Connor Turnbull <connorturnbull93@gmail.com>
2023-05-08 21:27:49 +0100

    create home page
    - created home page from wireframe
    - threads menu placeholder
    - content window placeholder
    - log in and sign up pop-up windows
    - no functionalities at present

--- src/App.js
+++ src/App.js
@@ -1,40 +1,54 @@
-//import logo from '';
 import React from 'react';
-import './App.css';
-import SideNav from './components/sideNav';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './App.css';
+
+import SideNav from './components/sideNav';
+import Login from './components/login/loginButton';
+
 import MainLogo from './icon-left-font-monochrome-black.svg';
-import { Navbar, Button, Col, Row, Container, } from 'react-bootstrap';
+import { Button, Col, Row, Container, } from 'react-bootstrap';
+import { useState } from 'react';
+import Signup from './components/login/signupButton';
 
 function App() {
   return (
     <div className='app' >
-      <Container className="header bg-primary" fluid >
+      <Container fluid className="header bg-primary"  >
         <Row className='p-3'>
           
-          <Col md ={2} lg={2} xl={2}>
+          <Col sm={2} md={2} lg={2} xl={2}>
             <img className='img-fluid' src={MainLogo} alt="The Groupomania company logo"/>
           </Col>
 
-          <Col md={8} lg={8} xl={8}>
+          <Col sm={8} md={8} lg={8} xl={8}>
 
           </Col>
 
-          <Col md={1} lg={1} xl={1}>
-            <Button className='p-1 text-nowrap' variant='light' size='sm'>Log In</Button>
+          <Col sm={1} md={1} lg={1} xl={1}>
+            <Login/>
           </Col>
 
-          <Col md={1} lg={1} xl={1}>
-            <Button className='p-1 text-nowrap' variant='light' size='sm'>Sign Up</Button>
+          <Col sm={1} md={1} lg={1} xl={1}>
+            <Signup/>
           </Col>
         
         </Row>
       </Container>
+      
+      <Container fluid className="main-container">
+        <Row>
+          
+          <Col className='thread-nav bg-secondary pb-3' md={2} lg={2} xl={2}>
+            <SideNav/>
+          </Col>
+          
+          <Col className='content-window d-flex justify-content-center align-items-center bg-light' md={10} lg={10} xl={10}>
+            'content window'
+          </Col>
 
-      <Container className="bg-secondary m-0" style={{width: '20%'}}>
-          <SideNav/>
+        </Row>  
       </Container>
+
     </div>
   );
 }
