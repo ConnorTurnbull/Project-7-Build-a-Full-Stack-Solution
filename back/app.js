0f390244ab1731ce343c331656040df00e1b1937
Connor Turnbull <connorturnbull93@gmail.com>
2023-08-22 21:00:05 +0100

    added db connection to app.js

--- back/app.js
+++ back/app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const path = require('path');
+const { Sequelize } = require('sequelize')
 
 const userRoutes = require('./routes/user');
 const threadRoutes = require('./routes/thread');
@@ -10,21 +11,30 @@ const commentRoutes = require('./routes/comment');
 const app = express();
 app.use(express.json());
 
+//Database Connection:
+const sequelize = new Sequelize('sqlite::memory:')
+
+sequelize.authenticate().then( () => {
+    console.log('Connection has been established successfully.');
+}).catch (error => {
+    console.error('Unable to connect to the database:', error);
+})
+
 //MongoDB connection:
-mongoose.connect('mongodb+srv://connorturnbull93:bLwcWErYEB8IjMYL@ct-project-6.xxrwq6d.mongodb.net/Project7?retryWrites=true&w=majority')
-    .then(() => {
-        console.log('Successfully connected to MongoDB Atlas!');
-    })
-    .catch((error) => {
-        console.log('Unable to connect to MongoDB Atlas!');
-        console.error(error);
-    });
+// mongoose.connect('mongodb+srv://connorturnbull93:bLwcWErYEB8IjMYL@ct-project-6.xxrwq6d.mongodb.net/Project7?retryWrites=true&w=majority')
+//     .then(() => {
+//         console.log('Successfully connected to MongoDB Atlas!');
+//     })
+//     .catch((error) => {
+//         console.log('Unable to connect to MongoDB Atlas!');
+//         console.error(error);
+//     });
 
 //Headers:
 app.use((req, res, next) => {
-    res.setHeader( 'Access-Control-Allow-Origin', '*');
-    res.setHeader( 'Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
-    res.setHeader( 'Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
 
